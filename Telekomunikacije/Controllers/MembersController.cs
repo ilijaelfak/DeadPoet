6910c0c4e3557fd3b7806207edc3882859a46c01
Ilija Đorđević <ilijaelfak@outlook.com>
2019-04-05 10:06:33 +0200

    Manage Roles

--- Telekomunikacije/Controllers/MembersController.cs
+++ Telekomunikacije/Controllers/MembersController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 using Telekomunikacije.Models;
 using Telekomunikacije.ViewModels;
 using Microsoft.AspNet.Identity.EntityFramework;
-
+using Microsoft.AspNet.Identity;
 
 namespace Telekomunikacije.Controllers
 {
@@ -83,32 +83,144 @@ namespace Telekomunikacije.Controllers
         public ActionResult RoleInfo(string Id)
         {
             var user = _Context.Users.Single(x => x.Id == Id);
-            
+
+            var roles = new List<IdentityRole>();
+
+            foreach (var item in user.Roles)
+            {
+                roles.Add(_Context.Roles.Single(x => x.Id == item.RoleId));
+            }
             
             var userRoles = new UserRoleViewModel
             {
                 UserName = user.UserName,
-                IdentityRoles = _Context.Roles.ToList(),
+                IdentityRoles = roles.ToList(),
                 Id = user.Id
             };
-
-            return View(userRoles);
+            var userRolesManage = new UserRoleManageViewModel
+            {
+                UserRoleViewModel = userRoles,
+                AllRoles = _Context.Roles.ToList()
+            };
+            return View(userRolesManage);
         }
 
-        public ActionResult AddRole(UserRoleViewModel user)
+        public ActionResult RoleAddToMember(UserRoleManageViewModel user,string Id)
         {
 
+            var userInDb = _Context.Users.Single(x => x.Id == user.UserRoleViewModel.Id);
+
+            var roles = new List<IdentityRole>();
+
+            foreach (var item in userInDb.Roles)
+            {
+                roles.Add(_Context.Roles.Single(x => x.Id == item.RoleId));
+            }
+            var roleName =_Context.Roles.Single(x => x.Id == Id).Name;
+
+            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(_Context));
+            manager.AddToRole(user.UserRoleViewModel.Id, roleName);
+
+            //var id = user.UserRoleViewModel.Id;
+            var p = new UserRoleViewModel
+            {
+                UserName = user.UserRoleViewModel.UserName,
+                IdentityRoles = roles.ToList(),
+                Id = user.UserRoleViewModel.Id
+            };
+            var p1 = new UserRoleManageViewModel
+            {
+                AllRoles = _Context.Roles.ToList(),
+                UserRoleViewModel = p
+            };
+            return View("RoleInfo", p1);
+        }
+
+        public ActionResult GetRoles(UserRoleManageViewModel user)
+        {
+
+            var userInDb = _Context.Users.Single(x => x.Id == user.UserRoleViewModel.Id);
+
+            var roles = new List<IdentityRole>();
+
+            foreach (var item in userInDb.Roles)
+            {
+                roles.Add(_Context.Roles.Single(x => x.Id == item.RoleId));
+            }
+
+            //var id = user.UserRoleViewModel.Id;
+            var p = new UserRoleViewModel
+            {
+                UserName = user.UserRoleViewModel.UserName,
+                IdentityRoles = roles.ToList(),
+                Id = user.UserRoleViewModel.Id
+            };
+            var p1 = new UserRoleManageViewModel
+            {
+                AllRoles = _Context.Roles.ToList(),
+                UserRoleViewModel = p
+            };
+            return View("RoleInfo",p1);
             
-            //  System.Web.Security.Roles.AddUserToRole(usernameID, choosenRole);
-            return RedirectToAction("Pomocni") ;
+        }
+
+        public ActionResult DeleteRoleForMember(UserRoleManageViewModel user, string Id)
+        {
+            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(_Context));
+
+            var roleName = _Context.Roles.Single(x => x.Id == Id).Name;
+
+            if (manager.IsInRole(user.UserRoleViewModel.Id, roleName))
+            {
+                manager.RemoveFromRole(user.UserRoleViewModel.Id, roleName);
+                ViewBag.ResultMessage = "Role removed from this user successfully !";
+            }
+            else
+            {
+                ViewBag.ResultMessage = "This user doesn't belong to selected role.";
+            }
+            var userInDb = _Context.Users.Single(x => x.Id == user.UserRoleViewModel.Id);
+
+            var roles = new List<IdentityRole>();
+
+            foreach (var item in userInDb.Roles)
+            {
+                roles.Add(_Context.Roles.Single(x => x.Id == item.RoleId));
+            }
+
+            //var id = user.UserRoleViewModel.Id;
+            var p = new UserRoleViewModel
+            {
+                UserName = user.UserRoleViewModel.UserName,
+                IdentityRoles = roles.ToList(),
+                Id = user.UserRoleViewModel.Id
+            };
+            var p1 = new UserRoleManageViewModel
+            {
+                AllRoles = _Context.Roles.ToList(),
+                UserRoleViewModel = p
+            };
+            return View("RoleInfo", p1);
         }
         public ActionResult Pomocni()
         {
 
-            var user = _Context.Users.Single(c => c.UserName == "Ilija");
-            System.Web.Security.Roles.AddUserToRole(user.Id, "CanTeach");
-            _Context.SaveChanges();
-            return View();
+            var id = _Context.Users.Single(x => x.UserName == "Admin").Id;
+            var user = new UserRoleViewModel
+            {
+                Id = id,
+                UserName = "Admin",
+                IdentityRoles = _Context.Roles.ToList()
+
+             };
+            var userRoleVM = new UserRoleManageViewModel
+            {
+                AllRoles = _Context.Roles.ToList(),
+                UserRoleViewModel = user
+            };
+
+            return View("RoleInfo", user);
+
 
 
         }
---
+++ Telekomunikacije/ViewModels/UserRoleManageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Telekomunikacije.ViewModels;
+using Telekomunikacije.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Telekomunikacije.ViewModels
+{
+    public class UserRoleManageViewModel
+    {
+        public UserRoleViewModel UserRoleViewModel  { get; set; }
+        public IEnumerable<IdentityRole> AllRoles { get; set; }
+    }
+}
